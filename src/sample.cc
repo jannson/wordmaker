8786b71dc8f482332bf7c1b20817464dee8f3f51
gan <wei_shenggan@szhnd.com>
2014-02-19 18:08:08 +0800

    todo for huge maker

--- src/sample.cc
+++ src/sample.cc
@@ -11,15 +11,25 @@
 int main() {
   marisa::Keyset keyset;
   keyset.push_back("a");
-  keyset.push_back("app");
   keyset.push_back("apple");
+  keyset.push_back("bpple");
+  keyset.push_back("apple");
+  keyset.push_back("appled");
+  keyset.push_back("app");
 
   marisa::Trie trie;
   trie.build(keyset);
-
+  
+  std::cout<<"common preffix search"<<std::endl;
   marisa::Agent agent;
   agent.set_query("apple");
   while (trie.common_prefix_search(agent)) {
+    std::cout.write(agent.key().ptr(), agent.key().length());
+    std::cout << ": " << agent.key().id() << std::endl;
+  }
+
+  std::cout<<"predictive search"<<std::endl;
+  while (trie.predictive_search(agent)) {
     std::cout.write(agent.key().ptr(), agent.key().length());
     std::cout << ": " << agent.key().id() << std::endl;
   }
